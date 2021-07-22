be0aef760e160f769d5742494dcf6f8fbd1669db
Justina Hortizuela <justinahortizuela@gmail.com>
2021-07-22 12:34:50 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Jackie Chan"
+is_active = true
+famous_person = "Ben Affleck"
+num_3 = 4.5
+num_9 = 13
+sum = num_9 + num_3
+animals = ["kitten", "puppy", "lizard"]
+animals.shift
+p famous_person.length
