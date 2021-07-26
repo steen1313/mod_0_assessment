981ac9541f5e50cc15de4d05301def395b2f8d92
Justina Hortizuela <justinahortizuela@gmail.com>
2021-07-26 13:58:01 -0700

    Add changes from a new file models

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+south_west_high = {
+  "principal" => "Donna Levy",
+  "vice principal" => "Donna Besser",
+  "student_amount" => 10000,
+  "building_amount" => 4,
+  "is_open" => true,
+  "is_old" => false,
+  "building_names" => ["a,", "b", "c", "d"],
+  "office_staff" => ["Lisa", "Tracy", "Amity",]
+}
+p south_west_high
---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+momo = {
+  "favorite_parent" => "Chaz",
+  "favorite_food" => "fancy feast",
+  "height" => 15,
+  "weight" => 9.43,
+  "is_adorable" => true,
+  "is_stinky" => false,
+  "favorite_toys" => ["donut", "sushi", "string"],
+  "bad_momo" => ["jump on tv", "eat plastic", "eat cardboard"]
+}
